e2ee3618b9f1dd828f2610cd43936a2d1109089e
kAzE <142893048+kr1th1k@users.noreply.github.com>
2026-09-03 08:25:00 +0530

    Delete area and circumference.c

--- area and circumference.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int r;
-    scanf("%d", &r);
-    int area = 3.14*r*r;
-    int circumference = 2*3.14*r;
-    printf("%d %d", area, circumference);
-    return 0;
-}
